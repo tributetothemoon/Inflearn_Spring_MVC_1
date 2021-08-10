b02ef77609f31cef9e2acdfc7ec086c62395c30b
Kimun Kim <kkm97351@gmail.com>
2021-08-11 00:20:34 +0900

    HelloServlet 실습

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan	// 서블릿을 스캔해서 자동으로 등록한다.
 @SpringBootApplication
 public class ServletApplication {
 
