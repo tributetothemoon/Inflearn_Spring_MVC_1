e3f19060f2034334f4ed411505ea749c4d6d4997
Kimun Kim <kkm97351@gmail.com>
2021-08-28 00:50:52 +0900

    Model 적용
    'Model 추가 - v3' 강의 내용 실습

---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberSaveControllerV3.java
@@ -0,0 +1,25 @@
+package hello.servlet.web.frontcontroller.v3.controller;
+
+import hello.servlet.domain.Member;
+import hello.servlet.domain.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.Map;
+
+public class MemberSaveControllerV3 implements ControllerV3 {
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        String username = paramMap.get("username");
+        int age = Integer.parseInt(paramMap.get("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelView modelView = new ModelView("save-result");
+        modelView.getModel().put("member", member);
+        return modelView;
+    }
+}
