c289bf263a13bb1455c41f004b171b5ba5bdf006
Kimun Kim <kkm97351@gmail.com>
2021-08-31 03:26:47 +0900

    뷰 리졸버 설정

---
+++ src/main/java/hello/servlet/MvcConfig.java
@@ -0,0 +1,18 @@
+package hello.servlet;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
